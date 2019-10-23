7189449c185b7d54ae44f01a9c28409fc3b800ec
Sam Hiatt <samhiatt@gmail.com>
2019-10-22 20:05:04 -0700

    Add code

---
+++ avian_vocalizations/utils.py
@@ -0,0 +1,139 @@
+import os, wget, re
+from zipfile import ZipFile
+import pandas as pd
+import numpy as np
+
+data_urls=[
+    "https://xeno-canto-ca-nv.s3.amazonaws.com/avian-vocalizations-partitioned-data.zip",
+    "https://xeno-canto-ca-nv.s3.amazonaws.com/xenocanto-avian-vocalizations-canv-usa.zip",
+    "https://xeno-canto-ca-nv.s3.amazonaws.com/avian-vocalizations-spectrograms-and-mfccs.zip",
+]
+train_index_filename = 'train_file_ids.csv'
+
+class ExceptionDataDirNotFound(Exception):
+    def __init__(self, data_dir, errors=None):
+        message = os.path.abspath(data_dir)+" not found. \n"+\
+                  "Download data by calling load_data with download_data=True"
+        super().__init__(message)
+        
+class ExceptionDataFileNotFound(Exception):
+    def __init__(self, file_path, errors=None):
+        message = file_path+" not found."
+        super().__init__(message)
+
+def load_data(data_dir='data', download_data=False ):
+    """
+    Args:
+        data_dir (string): Relative path pointing to data dir. 
+        download (bool):   Download data if not found. (Default: False)
+    Returns: index_df, with cols augmented to include:
+                train/test flag,
+                nFrames,
+                melspectrogram filename,
+                mfcc filename
+    Raises: 
+        ExceptionDataDirNotFound: if data_dir cannot be found.
+        ExceptionDataFileNotFound: if any of the required data files dannot be found. 
+    """
+    if not os.path.exists(data_dir):
+        if download_data:
+            download_data(data_dir)
+        else:
+            raise(ExceptionDataDirNotFound(data_dir))
+            
+    data_files=[
+        "xeno-canto_ca-nv_index.csv",
+        "feature_shapes.csv",
+        "train_file_ids.csv",
+        "test_file_ids.csv",
+    ]
+    # Make sure we have all the expected data files
+    for filename in data_files:
+        path = os.path.join(data_dir,filename)
+        if not os.path.exists(path): 
+            raise(ExceptionDataFileNotFound(path))
+            
+            
+    index_df = pd.read_csv("data/xeno-canto_ca-nv_index.csv", 
+                           index_col='file_id')
+    shapes_df = pd.read_csv(os.path.join(data_dir,"feature_shapes.csv"),
+                            index_col=0 )
+    train_df = pd.read_csv(os.path.join(data_dir,"train_file_ids.csv"),
+                           index_col=0)
+    test_df = pd.read_csv(os.path.join(data_dir,"test_file_ids.csv"),
+                          index_col=0)
+    
+    # Parse shapes (which were originally inadvertently saved as a string).
+    shapes_df['n_frames'] = [_parse_shape(row['mfcc_shapes'])[1] for i,row in shapes_df.iterrows() ]
+    # Add n_frames to index_df too
+    index_df['n_frames'] = shapes_df.loc[index_df.index==shapes_df['file_id'],'n_frames'].values
+    
+    # Indicate which files belong to the test dataset. 
+    index_df['test']=False
+    index_df.loc[test_df.index,'test']=True
+    
+    # Add paths to mfcc and melspectrogram mmapped files 
+    index_df['melspectrogram_path'] = [os.path.join(data_dir,'features','XC%s_melspectrogram.dat'%(file_id)) 
+                                       for file_id in index_df.index]
+    index_df['mfcc_path'] = [os.path.join(data_dir,'features','XC%s_mfcc.dat'%(file_id)) 
+                             for file_id in index_df.index]
+    
+    return index_df, shapes_df, train_df, test_df
+
+def get_mfcc_array(df, file_id):
+    rec = df.loc[file_id]
+    return np.memmap(rec.get('mfcc_path'), dtype='float32', mode='readonly', 
+                     shape=(20,rec.get('n_frames')))
+
+def get_melsg_array(df, file_id):
+    rec = df.loc[file_id]
+    return np.memmap(rec.get('melspectrogram_path'), dtype='float32', mode='readonly', 
+                     shape=(128,rec.get('n_frames')))
+
+def download_data(data_dir='data'):
+    if not os.path.exists(data_dir):
+        print("Creating data dir "+os.path.abspath(data_dir))
+        os.mkdir(data_dir)
+    for url in data_urls: _download_and_extract(url, data_dir)
+    # These files should have been included in the zip archives extracted above.
+    zip_file_destinations = {
+        "xeno-canto-ca-nv.zip": "audio",
+        "melspectrograms.zip": "features",
+        "mfccs.zip": "features",
+    }
+    for zip_filename in zip_file_destinations:
+        zip_filepath=os.path.join(data_dir, zip_filename)
+        with ZipFile(zip_filepath) as zf:
+            destination_dir=os.path.join(data_dir,zip_file_destinations[zip_filename])
+            if not os.path.exists(destination_dir): os.mkdir(destination_dir)
+            zf.extractall(destination_dir)
+            print("Extracted contents of %s to %s."%(zip_filename, destination_dir))
+        os.remove(zip_filepath)
+        
+        
+def _parse_shape(shape_str):
+    """Shape was saved in feature_shapes as a string. Woops.
+       Convenience funtion to parse out the values. """
+    if type(shape_str) is pd.Series:
+        return shape_str.map(_parse_shape)
+    a,b = re.search('\((\d+), (\d+)\)',shape_str).groups()
+    return int(a), int(b)
+        
+def _download_and_extract(url, data_dir, keep_zip=False):
+    print("Downloading and extracting "+url)
+    filename = url[url.rindex('/')+1:]
+    if not os.path.exists(filename): 
+        wget.download(url, out=filename)
+        print("Downloaded "+filename)
+    else:
+        print("Using existing file "+filename)
+        keep_zip=True # Don't delete a pre-existing file
+    with ZipFile(filename) as archive:
+        for file in archive.infolist():
+            archive.extract(file, data_dir)
+            print("Extracted %s."%(os.path.abspath(os.path.join(data_dir,file.filename))))
+    if not keep_zip: os.remove(xc_index_zip_filename)
+
+def log_clipped(a):
+    """Convenience function to clip the input to positive values then return the log.""" 
+    return np.log(np.clip(a,.0000001,a.max()))
---
+++ avian_vocalizations/visualization.py
@@ -0,0 +1,69 @@
+import os
+import librosa
+from librosa.display import specshow
+from IPython.display import Audio
+from matplotlib.gridspec import GridSpec, GridSpecFromSubplotSpec
+import matplotlib.pyplot as plt
+import numpy as np
+
+def show_sample(melsg, mfcc, file_id=None, label="", offset=0, data_dir='data', load_clip=False):
+    fig = plt.figure(figsize=(7,5))
+    if file_id or label!="":
+        fig.suptitle(' '.join([ ("XC%s"%file_id) if file_id else "", 
+                         label
+                       ]))
+    gs = GridSpec(4, 1, fig, hspace=.1, wspace=0, top=.93)
+    melsg_ax = fig.add_subplot(gs[0:3])
+    specshow(melsg.squeeze(), y_axis='mel', vmin=-3, vmax=3, ax=melsg_ax)
+    plt.colorbar(melsg_ax.collections[0], ax=melsg_ax, pad=.01)
+    mfcc_ax = fig.add_subplot(gs[3])
+    specshow(mfcc.squeeze(), ax=mfcc_ax, x_axis='s')
+    mfcc_ax.set_ylabel("MFCC")
+    mfcc_ax.set_yticks([0,5,10,15])
+    # TODO: Ensure 22050 is correct frame rate
+    mfcc_ax.set_xticklabels(["%0.1f"%(t+offset/(22050/512)) 
+                             for t in mfcc_ax.get_xticks()])
+    plt.colorbar(mfcc_ax.collections[0], ax=mfcc_ax, aspect=7, pad=.01)
+    plt.show()
+    if file_id and load_clip:
+        file_path = os.path.join(data_dir,'audio',"XC%s.mp3"%file_id)
+        print(file_path)
+        data, samplerate = librosa.load(file_path)
+        display(Audio(data, rate=samplerate))
+    
+    
+def vis_sample_with_histograms(sample, data_dir='data'):
+    data, sr = librosa.load(os.path.join(data_dir,'audio',sample.file_name))
+    Audio(data, rate = sr)
+    hop_length = 512
+    fmin = 0
+    melsg = librosa.feature.melspectrogram(data, sr=sr, hop_length=hop_length, n_fft=2048, fmin=fmin)
+    mfcc = librosa.feature.mfcc(data, sr=sr, hop_length=hop_length, n_fft=2048, fmin=fmin)
+    fig = plt.figure(figsize=(15,5)); #plt.subplots_adjust(hspace=4)
+    cols = GridSpec(1,2, fig) # get a grid of 1 row x 2 cols
+    left_col = GridSpecFromSubplotSpec(3,1,subplot_spec=cols[0], hspace=1)
+    right_col = GridSpecFromSubplotSpec(5,1,subplot_spec=cols[1], hspace=3)
+    fig.suptitle("%s: %s"%(sample.file_name, sample.full_name))
+    hist_ax1 = fig.add_subplot(left_col[0], xlabel="Power",
+                               title="Histogram of Melspectrogram Pixels")
+    hist_ax2 = fig.add_subplot(left_col[1], xlabel="log(Power)",
+                               title="Histogram of log(Melspectrogram)")
+    mfcc_hist_ax = fig.add_subplot(left_col[2], xlabel="MFCC",
+                               title="Histogram of MFCC Pixels")
+    melsg_ax = fig.add_subplot(right_col[:3])
+    mfcc_ax = fig.add_subplot(right_col[3:])
+    hist_ax1.hist(melsg.flatten(), bins=100)
+    hist_ax2.hist(np.log(melsg.flatten()), bins=100)
+    mfcc_hist_ax.hist(mfcc.flatten(), bins=100)
+    specshow(np.log(melsg), y_axis='mel', x_axis='s', ax=melsg_ax, fmin=fmin,
+             hop_length=hop_length, sr=sr)
+#     specshow(librosa.amplitude_to_db(melsg[50:,:], ref=np.max), y_axis='mel', x_axis='s', ax=melsg_ax, 
+#              hop_length=hop_length, sr=sr, fmin=1376)
+    melsg_ax.set_title("log(Melspectrogram)")
+    plt.colorbar(melsg_ax.get_children()[0], ax=melsg_ax)
+    specshow(mfcc, x_axis='s', ax=mfcc_ax, fmin=fmin, 
+             hop_length=hop_length, sr=sr)
+    mfcc_ax.set_title("Mel-frequency Cepstral Coefficients")
+    plt.colorbar(mfcc_ax.get_children()[0], ax=mfcc_ax);
+    display(fig)
+    plt.close('all')
